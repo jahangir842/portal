57440dbca3861f9c3efa38c5026f38bb51e7379e
Jagz <144284208+jahangir842@users.noreply.github.com>
2025-04-15 20:46:04 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,16 @@
-feat: Terraform directory added
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "portal-rg"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "portalacr2025" # must be globally unique
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic" # options: Basic, Standard, Premium
+  admin_enabled       = true    # enables admin username/password login
+}
